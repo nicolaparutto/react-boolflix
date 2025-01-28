1d4bf57b17267c296e9dc81c0a999a1d288bc261
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-28 18:22:56 +0100

    feat: added logic for converting vote number to integer

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -3,6 +3,12 @@ import flags from "../assets/data/languagesFlags";
 function FilmCard(props) {
    const element = props.filmContent;
 
+   //Logica per convertire il numero decimale:
+   let filmVote = element.vote_average;
+   let voteConversion = Math.ceil(filmVote / 2);
+   //Logica per gestire le stelle di valutazione:
+   //.......
+
    return (
       <>
          <div className="film-series-card">
@@ -10,9 +16,10 @@ function FilmCard(props) {
                <h1>{element.title}</h1>
                <h2>{element.original_title}</h2>
                <p>Language: {element.original_language}</p>
-               <p>Voto Medio: {element.vote_average}</p>
+               <p>Voto Medio: {voteConversion}</p>
                <img src={flags[element.original_language]} alt="" />
                <img src={`https://image.tmdb.org/t/p/w200${element.poster_path}`} alt="" />
+               <span>STARS</span>
             </div>
          </div>
       </>
