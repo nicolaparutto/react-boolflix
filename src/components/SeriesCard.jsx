66079d6f4e7221bf58090e3c060fee9322a613a5
nicolaparutto <nicolaparutto2003@gmail.com>
2025-02-01 14:50:42 +0100

    feat: changed and fixed the card overlay system

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -8,30 +8,34 @@ function SeriesCard(props) {
 
    const [imgSrc, setImgSrc] = useState(`https://image.tmdb.org/t/p/w200${element.poster_path}`)
 
+   const [hover, setHover] = useState(false)
+
    return (
 
       <>
          <div className="film-series-card">
-            <div className="card-content">
+            <div className="card-content" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
                <div className="card-image">
                   <img src={imgSrc} alt="" onError={() => setImgSrc("img/placeholder-img.jpg")} />
                </div>
 
-               <div className="card-info">
-                  <h2>{element.name}</h2>
-                  <div className="language-info">
-                     <span>Lingua:</span>
-                     <div className="flag-container">
-                        <img src={flags[element.original_language]} alt={element.original_language} />
+               <div className={`overlay ${hover ? "show" : ""}`}>
+                  <div className="card-info">
+                     <h2>{element.original_name}</h2>
+                     <div className="language-info">
+                        <span>Lingua:</span>
+                        <div className="flag-container">
+                           <img src={flags[element.original_language]} alt={element.original_language} />
+                        </div>
                      </div>
-                  </div>
-                  <div className="rating-info">
-                     <span>Valutazione media:</span>
-                     <StarsRating vote={element.vote_average} />
-                  </div>
-                  <div className="overview-info">
-                     <span>Trama:</span>
-                     <div className="overview-text">{element.overview}</div>
+                     <div className="rating-info">
+                        <span>Valutazione media:</span>
+                        <StarsRating vote={element.vote_average} />
+                     </div>
+                     <div className="overview-info">
+                        <span>Trama:</span>
+                        <div className="overview-text">{element.overview}</div>
+                     </div>
                   </div>
                </div>
             </div>
